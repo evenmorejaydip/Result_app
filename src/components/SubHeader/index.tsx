3e5366e75b8c3a809be164e6060e426a239ae005
shreeji <solutionkmitjaydip@gmail.com>
2024-04-08 18:00:53 +0530

    design fix

--- src/components/SubHeader/index.tsx
+++ src/components/SubHeader/index.tsx
@@ -5,44 +5,38 @@ import Image from "next/image";
 const SubHeader = () => {
   return (
     <header className="sticky top-0 z-999 flex w-full bg-white drop-shadow-1 dark:bg-boxdark dark:drop-shadow-none">
-      <div className="flex flex-grow items-center justify-end px-4 py-4 shadow-2 md:px-6 2xl:px-11">
-        <div className="flex items-center gap-2 sm:gap-4 lg:hidden">
-          <Link className="block flex-shrink-0 lg:hidden" href="/">
+      <div className="flex flex-grow items-center justify-between	 px-4 py-4 shadow-2 md:px-6 2xl:px-11">
+        <div className="flex items-center gap-2 sm:gap-4">
+          <Link href="/">
             <Image
-              width={32}
+              width={176}
               height={32}
-              src={"/images/logo/logo-icon.svg"}
+              src={"/images/logo/logo.svg"}
               alt="Logo"
+              priority
             />
+
+            <h2 className="text-[20px] font-semibold text-black dark:text-white sm:text-title-md2">
+              Gundaran Parivar
+            </h2>
           </Link>
         </div>
 
         <div className="flex items-center justify-end gap-3 2xsm:gap-7">
-          <ul className="flex items-center gap-2 2xsm:gap-4">
+          <ul className="flex hidden items-center gap-2 2xsm:gap-4 lg:block">
             {/* <!-- Dark Mode Toggler --> */}
             <DarkModeSwitcher />
           </ul>
 
           <div className="relative">
             <Link className="flex items-center gap-4" href="#">
-              <span className="hidden text-right lg:block">
-                <span className="block text-sm font-medium text-black dark:text-white">
+              <span className="text-right">
+                <span className="block text-[20px] font-semibold text-black dark:text-white sm:text-title-md2">
                   Jaydip Mangukiya
                 </span>
-                <span className="block text-xs">Developed by</span>
-              </span>
-
-              <span className="h-12 w-12 rounded-full">
-                <Image
-                  width={112}
-                  height={112}
-                  src={"/images/user/user-01.png"}
-                  style={{
-                    width: "auto",
-                    height: "auto",
-                  }}
-                  alt="User"
-                />
+                <span className="block text-[16px] font-semibold text-black">
+                  Developed by
+                </span>
               </span>
             </Link>
           </div>
