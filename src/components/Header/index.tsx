4060290b65bc6b22ec0cbe69e1f2368f756063e9
shreeji <solutionkmitjaydip@gmail.com>
2024-04-20 15:49:27 +0530

    print added

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -53,15 +53,6 @@ const Header = (props: {
             </span>
           </button>
           {/* <!-- Hamburger Toggle BTN --> */}
-
-          <Link className="block flex-shrink-0 lg:hidden" href="/">
-            <Image
-              width={32}
-              height={32}
-              src={"/images/logo/logo-icon.svg"}
-              alt="Logo"
-            />
-          </Link>
         </div>
 
         <div className="flex items-center gap-3 2xsm:gap-7">
