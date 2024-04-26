41c634e40edca3904ae6cd5b8267e1a72f924018
shreeji <solutionkmitjaydip@gmail.com>
2024-04-26 19:35:20 +0530

    error solve

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,11 +1,15 @@
 module.exports = {
   apps: [
     {
-      name: "nextjs-app",
-      script: "npm",
-      args: "run build",
+      name: "your-nextjs-app",
+      script: "npm", // Path to the Next.js binary
+      args: "start", // Arguments to pass to the Next.js binary (in this case, "start" to start the Next.js server)
+      instances: "1", // Use as many instances as the number of CPU cores
+      autorestart: true, // Automatically restart the application if it crashes
+      watch: false, // Set to false for Next.js applications as they handle file changes internally
+      max_memory_restart: "1G", // Restart the app if it exceeds 1GB memory usage
       env: {
-        NODE_ENV: "production",
+        NODE_ENV: "production", // Set your desired environment variables here
       },
     },
   ],
